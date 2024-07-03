d4412710a91f5fcf8dd8e8e60e99bcbce52d9b7b
Miranda <77069174+MagadeOz@users.noreply.github.com>
2024-07-03 21:20:43 +0000

    indentation

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
-const { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound  } = require('./app.js');
+const { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
-test("One euro should be 1.07 dollars", function() {
+test("One euro should be 1.07 dollars", function () {
     const dollars = fromEuroToDollar(3.5);
-    const expected = 3.5 * 1.07; 
-     expect(dollars).toBe(expected);
+    const expected = 3.5 * 1.07;
+    expect(dollars).toBe(expected);
 })
 
-test("One euro should be 547.75 yens", function() {
+test("One euro should be 547.75 yens", function () {
     const dollars = fromDollarToYen(3.5);
-    const expected = 3.5 * 156.5; 
-     expect(dollars).toBe(expected); 
+    const expected = 3.5 * 156.5;
+    expect(dollars).toBe(expected);
 })
 
-test("One yen should be 3.045 pounds", function() {
+test("One yen should be 3.045 pounds", function () {
     const pounds = fromYenToPound(3.5);
-    const expected = 3.5 * 0.87; 
-     expect(pounds).toBe(expected); 
+    const expected = 3.5 * 0.87;
+    expect(pounds).toBe(expected);
 })
